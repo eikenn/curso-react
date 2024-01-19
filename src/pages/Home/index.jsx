d5c276654c068c05ee0c2e67d4bd655f37d9a3a8
Vitor Eiken <vitoreiken@gmail.com>
2024-01-19 19:48:35 -0300

    feat: Home page and Pokemon styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-import { Pokemon } from '../../components/Pokemon/index'
+import { Pokemon } from '../../components/Pokemon'
+import PokemonLogo from '../../assets/images/pokemon_logo.png'
+import styles from './styles.module.css'
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Meu site de Pokemon</h1>
+    <div className={styles.container}>
+      <img className={styles.image} src={PokemonLogo} alt="Pokemon Logo" />
       <Pokemon name="Pikachu"/>
       <Pokemon name="Charmander"/>
       <Pokemon name="Charizard"/>
